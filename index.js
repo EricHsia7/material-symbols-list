e3451fff82867d297abd50cc4fc786b242a80d88
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-08-22 23:32:18 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const fs = require('fs');
 const pako = require('pako');
 
+async function makeDirectory(path) {
+  // Check if the path already exists
+  try {
+    await fs.promises.access(path);
+    // If there is no error, it means the path already exists
+    console.log('Given directory already exists!');
+  } catch (error) {
+    // If there is an error, it means the path does not exist
+    // Try to create the directory
+    try {
+      await fs.promises.mkdir(path, { recursive: true });
+      // If there is no error, log a success message
+      console.log('New directory created successfully!');
+    } catch (error) {
+      // If there is an error, log it
+      console.error(error);
+      process.exit(1);
+    }
+  }
+}
+
 async function fetchLatestVersionJson() {
   const url = `https://raw.githubusercontent.com/marella/material-symbols/main/_data/versions.json?_${new Date().getTime()}`;
 
@@ -19,13 +40,6 @@ async function fetchLatestVersionJson() {
   }
 }
 
-function compressStringToGzip(inputString, outputFilePath) {}
-
-const inputString = 'This is the string to compress into gzip.';
-const outputFilePath = 'archive.gz';
-
-compressStringToGzip(inputString, outputFilePath);
-
 async function main() {
   var json = await fetchLatestVersionJson();
   var list = [];
@@ -33,11 +47,11 @@ async function main() {
     list.push(key);
   }
   const text = list.join(',');
-  const outputFilePath = './dist/index.gz';
   // Compress the input string using pako
   const compressedData = pako.gzip(inputString);
   // Save the compressed data to a .gz file
-  await fs.promises.writeFile(outputFilePath, Buffer.from(compressedData));
+  await makeDirectory('./dist');
+  await fs.promises.writeFile('./dist/index.gz', Buffer.from(compressedData));
   process.exit(0);
 }
 
