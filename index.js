5c3fdd0e27cba1bd4fe7845b0354acb0f9d65e77
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-19 18:14:22 +0800

    update

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 const fs = require('fs');
 const pako = require('pako');
-
-async function makeDirectory(path) {
-  // Check if the path already exists
-  try {
-    await fs.promises.access(path);
-    // If there is no error, it means the path already exists
-    console.log('Given directory already exists!');
-  } catch (error) {
-    // If there is an error, it means the path does not exist
-    // Try to create the directory
-    try {
-      await fs.promises.mkdir(path, { recursive: true });
-      // If there is no error, log a success message
-      console.log('New directory created successfully!');
-    } catch (error) {
-      // If there is an error, log it
-      console.error(error);
-      process.exit(1);
-    }
-  }
-}
+const { makeDirectory } = require('./files.js');
 
 async function fetchLatestVersionJson() {
   const url = `https://raw.githubusercontent.com/marella/material-symbols/main/_data/versions.json?_${new Date().getTime()}`;
---
+++ rasterize.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const { Resvg } = require('@resvg/resvg-js');
+const { Jimp } = require('jimp');
+const { makeDirectory, getFiles } = require('./files.js');
+
+async function readSVGAsBuffer(filePath) {
+  try {
+    const absolutePath = path.resolve(filePath);
+
+    // Read the file as UTF-8 string
+    const svgText = await fs.promises.readFile(absolutePath, 'utf-8');
+
+    // Convert modified text to Buffer
+    const svgBuffer = Buffer.from(svgText, 'utf-8');
+
+    return svgBuffer;
+  } catch (err) {
+    console.error('Error reading or modifying SVG:', err);
+    throw err;
+  }
+}
+
+async function rasterize(filePath, outputDir, size = 128, scale = 3) {
+  const svg = await readSVGAsBuffer(filePath);
+  const options = {
+    background: '#ffffff',
+    fitTo: {
+      mode: 'width',
+      value: size * scale
+    }
+  };
+  const resvg = new Resvg(svg, options);
+  const pngData = resvg.render();
+  const pngBuffer = pngData.asPng();
+  const resizedImage = await Jimp.fromBuffer(pngBuffer);
+  resizedImage.resize({ w: size, h: size });
+  const extension = path.extname(filePath);
+  const newName = `${path.basename(filePath, extension)}.png`;
+  await resizedImage.write(path.join(outputDir, newName));
+}
+
+async function main() {
+  const outputDir = './tmp/rasterized/';
+  await makeDirectory(outputDir);
+  const files = await getFiles('./tmp/updated/');
+  for (const file of files) {
+    await rasterize(file.path.full, outputDir);
+    console.log(`Successfully rasterized ${file.path.name}.`);
+  }
+
+  process.exit(0);
+}
+
+main();
