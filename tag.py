0a1aca077f1388ea4374ae8fb83cb3ad5a4d5c7d
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-08-19 20:34:09 +0800

    update

--- tag.py
+++ tag.py
@@ -18,8 +18,12 @@ def get_class_descriptions():
 
 device = "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
+
 class_descriptions = get_class_descriptions()
-text_inputs_1 = torch.cat([clip.tokenize(desc) for desc in class_descriptions]).to(device)
+text_inputs = torch.cat([clip.tokenize(desc) for desc in class_descriptions]).to(device)
+with torch.no_grad:
+  text_features = model.encode_text(text_inputs)
+  text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
 def tag_image(file_path):
   start = time.time()
@@ -28,23 +32,20 @@ def tag_image(file_path):
     image_features = model.encode_image(image)
     image_features = image_features / image_features.norm(dim=-1, keepdim=True)
 
-    text_features_1 = model.encode_text(text_inputs_1)
-    text_features_1 = text_features_1 / text_features_1.norm(dim=-1, keepdim=True)
-
-    similarities = (100.0 * image_features @ text_features_1.T).softmax(dim=-1)
+    similarities = (100.0 * image_features @ text_features.T).softmax(dim=-1)
     values, indices = similarities[0].topk(32)
-    result_1 = []
+    result = []
     for idx, val in zip(indices, values):
-      result_1.append(class_descriptions[idx])
-    
-    text_inputs_2 = torch.cat([clip.tokenize(desc) for desc in result_1]).to(device)
-    text_features_2 = model.encode_text(text_inputs_2)
+      result.append(class_descriptions[idx])
 
-    similarities_2 = (100.0 * image_features @ text_features_2.T).softmax(dim=-1)
+    text_inputs_1 = torch.cat([clip.tokenize(desc) for desc in result]).to(device)
+    text_features_1 = model.encode_text(text_inputs_1)
+
+    similarities_2 = (100.0 * image_features @ text_features_1.T).softmax(dim=-1)
     values_2, indices_2 = similarities_2[0].topk(8)
     result_2 = []
     for idx, val in zip(indices_2, values_2):
-      result_2.append(result_1[idx])
+      result_2.append(result[idx])
     end = time.time()
     print(f"Successfully tagged {file_path} in {end - start:.2f}s")
     return result_2
