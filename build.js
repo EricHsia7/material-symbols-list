3cef1ca40636cf7af6806ea5d0cfc5451a68f106
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-04-25 18:49:24 +0800

    update

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { makeDirectory, writeTextFile, getFiles, readFile } = require('./files.js');
 const pako = require('pako');
+const emojiRegex = require('emoji-regex');
 
 async function main() {
   const versions = require('./versions.json');
@@ -45,7 +46,7 @@ async function main() {
     const content = await readFile(file.path.full);
     const fileContentWords = content
       .split(/[;,\n\s]+/g)
-      .filter((e) => e !== '' && !/\[[a-z0-9]+|(->)|[\u4E00-\u9FFF]+|[\u1F300-\u1F9FF]+/g.test(e))
+      .filter((e) => e !== '' && !/\[[a-z0-9]+|(->)|[\u4E00-\u9FFF]+|[\u1F300-\u1F9FF]+/g.test(e) && !emojiRegex().text(e))
       .map((e) => e.replace(/\p{Cc}/gu, '').toLowerCase());
 
     // Keep the semantic frequency
