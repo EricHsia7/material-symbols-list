15b4cfa6e8f13a907946328e8c77fd15056d7d46
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-04-29 23:05:53 +0800

    update

--- build.js
+++ build.js
@@ -46,7 +46,7 @@ async function main() {
     const content = await readFile(file.path.full);
     const fileContentWords = content
       .split(/[;,\n\s\_\/]+/g)
-      .filter((e) => e !== '' && !/\[[a-z0-9]+|(->)|[\u4E00-\u9FFF]+|[\~\*\{\}\!\#\@\$\`]+/g.test(e) && !emojiRegex().test(e))
+      .filter((e) => e !== '' && !/\[[a-z0-9]+|(->)|[\u4E00-\u9FFF]+|[\~\*\{\}\!\#\@\$\`\>\<]+/g.test(e) && !emojiRegex().test(e))
       .map((e) => e.replace(/\p{Cc}/gu, '').toLowerCase());
 
     // Keep the semantic frequency
