ca6ceccb4d34fc45ef9488b9c825b9c51fe948eb
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-05-24 12:55:42 +0800

    update

--- similarity.py
+++ similarity.py
@@ -39,7 +39,22 @@ def read_input_file(input_file: str, number_type: np.dtype = np.float32):
       # embedding = embedding / np.linalg.norm(embedding)
       embeddings[idx, :] = embedding
       idx += 1
-    return {"keys": keys, "dictionary": data['dictionary'], "embeddings": embeddings}
+    return {"keys": keys, "dictionary": dictionary, "embeddings": embeddings}
+
+def key_to_symbol_name(key, dictionary):
+  components_str = key.split('_')
+  words = [dictionary[int(x, 36)] for x in components_str]
+  return "_".join(words)
+
+def to_radix_string(n, radix):
+    if n == 0:
+        return "0"
+    digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    while n > 0:
+        result = digits[n % radix] + result
+        n //= radix
+    return result
 
 def main(number_type: torch.dtype = torch.float32, top_k: int = 16, threshold: float = 0.5, input_file = "./dist/search-index.json", output_dir = "./dist"):
   dev = pick_device()
@@ -52,17 +67,35 @@ def main(number_type: torch.dtype = torch.float32, top_k: int = 16, threshold: f
   sim = x @ x.T
   sim.fill_diagonal_(-float('inf'))
   top_sims, top_indices = torch.topk(sim, k = top_k, dim = 1, sorted = True)
-  result = {"dictionary": dictionary, "similarity": {}}
+  frequency_map = {}
+  similarity = {}
+  result = {"symbols": [], "similarity": {}}
   for idx, (sim_items, scores) in enumerate(zip(top_indices, top_sims)):
     matched_ids = sim_items.tolist()
     matched_scores = scores.tolist()
     arr = []
     for i, matched_id in enumerate(matched_ids):
       if matched_scores[i] >= threshold:
-        arr.append(keys[matched_id])
+        symbol_name = key_to_symbol_name(keys[matched_id], dictionary)
+        if symbol_name not in frequency_map:
+          frequency_map[symbol_name] = 0
+        frequency_map[symbol_name] += 1
+        arr.append(symbol_name)
+    
     if len(arr) > 0:
-      result["similarity"][keys[idx]] = ",".join(arr)
-
+      symbol_name = key_to_symbol_name(keys[idx], dictionary)
+      if symbol_name not in frequency_map:
+        frequency_map[symbol_name] = 0
+      frequency_map[symbol_name] += 1
+      similarity[symbol_name] = arr
+  symbols = sorted(frequency_map.items(), key = lambda item: item[1], reverse = True)
+  symbols = [x[0] for x in symbols]
+  for symbol_name in similarity:
+    arr = similarity[symbol_name]
+    arr = [to_radix_string(symbols.index(x), 36) for x in arr]
+    key = to_radix_string(symbols.index(symbol_name), 36)
+    result["similarity"][key] = ",".join(arr)
+  result["symbols"] = ",".join(symbols)
   output_file = os.path.join(output_dir, "similarity.json")
   with open(output_file, 'w') as g:
     json.dump(result, g, ensure_ascii = False, separators = (',', ':'))
