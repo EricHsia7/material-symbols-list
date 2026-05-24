e9538729c32116cb7df53458bf0ac4ea7f40c12e
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-05-24 11:54:12 +0800

    update

--- similarity.py
+++ similarity.py
@@ -63,8 +63,6 @@ def main(number_type: torch.dtype = torch.float32, top_k: int = 16, threshold: f
     if len(arr) > 0:
       result["similarity"][keys[idx]] = ",".join(arr)
 
-  print(result)
-
   output_file = os.path.join(output_dir, "similarity.json")
   with open(output_file, 'w') as g:
     json.dump(result, g, ensure_ascii = False, separators = (',', ':'))
