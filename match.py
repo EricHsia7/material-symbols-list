3cbb4991558b4518e6c507ec62aa78f6254ae164
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-06-13 01:47:59 +0800

    update

--- match.py
+++ match.py
@@ -2,14 +2,23 @@ import clip, torch
 from PIL import Image
 import time
 import sys
+import json
 
 device = "cpu"
 model, preprocess = clip.load("ViT-B/16", device=device)
 
 def main():
+  class_descriptions = []
+  try:
+    raw_input = sys.stdin.read()
+    data = json.loads(raw_input)
+    class_descriptions = data.get('descriptions')
+  except Exception as e:
+    error_response = {"status": "error", "message": str(e)}
+    print(json.dumps(error_response))
+  
   arguments = sys.argv[1:]
   image_path = arguments[0]
-  class_descriptions = arguments[1:]
 
   image_tensor = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
   with torch.no_grad():
@@ -26,7 +35,7 @@ def main():
 
     result = []
     for idx, val in zip(indices, values):
-      result.append(f" - {class_descriptions[idx]} (cosine similarity: {val:.5f})")
+      result.append(f" - {class_descriptions[idx]} (similarity: {val:.5f})")
     result_text = "\n".join(result)
     report = f"""Matched result:
 {result_text}"""
